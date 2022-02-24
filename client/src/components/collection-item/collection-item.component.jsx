0b7fc62d9b40362319a96b6414962553e4639bc1
Alejandro Rodriguez <aleronal@hotmail.com>
2022-02-24 23:32:04 +0000

    lessons about useContext

--- client/src/components/collection-item/collection-item.component.jsx
+++ client/src/components/collection-item/collection-item.component.jsx
@@ -16,6 +16,8 @@ const CollectionItem = ({item, addItem}) => {
                 backgroundImage: `url(${imageUrl})`
             }} >
         </div>
+
+        
         <div className='collection-footer'>
             <span className='name'>{name}</span>
             <span className='price'>{price}</span>
